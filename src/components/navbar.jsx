b03a9bf41acca4075c82c77f6cfb91dfc00aa1d4
Patrick <65928079+HippoBaguette@users.noreply.github.com>
2021-04-29 16:56:38 +0800

    updated navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,17 +4,23 @@ class Navbar extends Component {
     render() {
         return (
 
-            <nav className="navbar navbar-light bg-light">
-                    <a className="navbar-brand" href="https://github.com/HippoBaguette">
-
-                        Meaningful and thought out shop name
-                        
-                        <div className="btn btn-warning m-2">
-                            <span >Your Cart:</span>
-                            <span className="badge badge-pill badge-success m-1">{this.props.totalCounters}</span>
-                        </div>
-                        
-                    </a>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="https://github.com/HippoBaguette">Navbar</a>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="https://github.com/HippoBaguette">Home</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="https://github.com/HippoBaguette">Features</a>
+                    </li>
+                 </ul>
+                </div>
+                 </div>
             </nav>
 
         )
