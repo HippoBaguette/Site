771241ca33b07dfcb7d3aa67f5f9dc65e49d5791
Patrick <65928079+HippoBaguette@users.noreply.github.com>
2021-04-29 18:52:04 +0800

    e

---
+++ src/components/content.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Content extends Component {
+    state = {  }
+    render() { 
+        return (
+            <img src="public/5rsow6fjhqw11.jpg" class="img-fluid" alt="e"></img>
+          );
+    }
+}
+ 
+export default Content;
